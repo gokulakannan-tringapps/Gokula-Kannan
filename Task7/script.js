ae74ad159f03a79b4381d3d7670c0c96990aeb2a
Gokula Kannan L <96760784+theSuperone-24@users.noreply.github.com>
2022-04-07 18:34:32 +0530

    Create script.js

---
+++ Task7/script.js
@@ -0,0 +1,15 @@
+
+function searchFun()
+{
+    var key = document.querySelector("#search").value.toUpperCase();
+    var myFood = document.querySelectorAll(".food");
+    for (let myfood of myFood) {
+        var inItems = myfood.querySelector("h2").innerText.toUpperCase();
+        if (inItems.indexOf(key) > -1){
+            myfood.style.display = "";
+        } 
+        else {
+            myfood.style.display = "none";
+        }
+    }
+}
